769c1ab77f8cdbeacb9775d68e4fdfc5e3eb4b28
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2019-01-21 14:25:54 -0500

    added layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  meals = [
+    'Argentine chimichuri steak',
+    'Balsamic fig chicken',
+    'Chicken piccata',
+    'Creamy chicken tortellini',
+    'Moroccan chicken',
+    'Pesto chicken',
+  ];
 }
